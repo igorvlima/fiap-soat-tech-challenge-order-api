745bfd4ef7d70c53704a48c678bfcca293a8dab9
igorvlima <igorveraslima3@gmail.com>
2025-07-28 10:02:13 -0300

    add annotation EnabledIfSystemProperty

--- src/test/java/com/example/fiapsoattechchallengeorderapi/FiapSoatTechChallengeOrderApiApplicationTests.java
+++ src/test/java/com/example/fiapsoattechchallengeorderapi/FiapSoatTechChallengeOrderApiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.fiapsoattechchallengeorderapi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnabledIfSystemProperty(named = "spring.profiles.active", matches = "local|dev|test")
 class FiapSoatTechChallengeOrderApiApplicationTests {
 
     @Test
